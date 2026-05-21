9d1c3f5b3a4a9b321bef97c4e4cab78e965d784d
Glen Egbert <glenegbert1@gmail.com>
2026-05-21 13:51:03 -0600

    Photo gallery index page (#12)
    * photo gallery index page
    
    * update workflow and some rails conventions

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,6 @@
 class PhotosController < ApplicationController
   def index
+    @photos = Photo.with_likes_count
+    @liked_photo_ids = current_user.liked_photo_ids
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ApplicationRecord
+  scope :with_likes_count, -> { left_joins(:likes).select("photos.*, COUNT(likes.id) AS likes_count").group(:id) }
+
   validates :photographer, :src_medium, :source_url, presence: true
   has_many :likes, dependent: :destroy
   has_many :liking_users, through: :likes, source: :user
